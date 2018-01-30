34082f38748072470307917d021c2aba4acae540
braca86 <braca86@gmail.com>
2018-01-30 17:38:11 +0100

    bootstrap-vue, top menu, main menu

---
+++ src/components/top-navigation/components/main-menu.component.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="main-menu-component">
+    <img src="./../../../assets/main-menu/blob.png" id="c7-menu-blob">
+    <img src="./../../../assets/main-menu/blob2.png" id="c7-menu-blob2">
+    <div class="container c7-menu">
+      <div class="row">
+          <!-- First Column Start -->
+          <div class="col-xl-3 col-md-4 col-sm-8 c7-hr c7-menu-c1 order-last order-md-first">
+            <ul class="list-unstyled c7-location">
+              <li class="c7-li-main g-bold">Switzerland</li>
+              <li class="g-light" >Brauerstrasse 11,</li>
+              <li class="g-light" >88088 Zurich</li>
+            </ul>
+            <ul class="list-unstyled">
+              <li class="c7-li-main g-bold">Serbia</li>
+              <li class="g-light" >Kumanovska 14,</li>
+              <li class="g-light" >11000 Belgrade</li>
+            </ul>
+            <ul class="list-unstyled c7-social">
+              <li><a class="g-bold" href="#">Facebook</a></li>
+              <li><a class="g-bold" href="#">LinkedIn</a></li>
+              <li><a class="g-bold" href="#">Dribble</a></li>
+              <li><a class="g-bold" href="#">Behence</a></li>
+              <li><a class="g-bold" href="#">Instagram</a></li>
+            </ul>
+          </div>
+          <!-- First Column End -->
+
+          <!-- Second Column Start -->
+          <div class="col-xl-3 col-md-4 col-sm-8 c7-menu-c2 order-first order-md-last">
+            <ul class="list-unstyled">
+              <li class="g-book" ><a href="#">Home</a></li>
+              <li class="g-book" ><a href="#">The people</a></li>
+              <li class="g-book" ><a href="#">Portfolio</a></li>
+              <li class="g-book" ><a href="#">Careers</a></li>
+              <li class="g-book" ><a href="#">Contact</a></li>
+            </ul>
+            <hr>
+          </div>
+          <!-- Second Column End -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
