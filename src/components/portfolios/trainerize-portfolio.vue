112c81b3889418726823fa39930aa7aeb725291c
concept7 <team@concept7.io>
2018-06-02 14:52:52 +0200

    trainerize portfolio page

--- src/components/portfolios/trainerize-portfolio.vue
+++ src/components/portfolios/trainerize-portfolio.vue
@@ -4,7 +4,7 @@
       <section class="portfolio-top-section">
         <div class="container">
           <div class="row">
-            <div class="col-12 text-center">
+            <div class="col-12 col-lg-10 offset-lg-1 text-center foo">
               <h1 class="portfolio-main-title">trainerize</h1>
               <p class="portfolio-main-subtitle">
                 Serving more than 17k personal trainers and total of 250k users,
@@ -22,19 +22,23 @@
         <div class="container">
           <div class="row">
             <h2 class="col-12 portfolio-section-main-title text-center text-white">Dashboard overview</h2>
+          </div>
+          <div class="row align-items-lg-center">
             <div class="col-12 col-lg-4">
-              <h3 class="portfolio-section-title text-white">Widget system</h3>
+              <h3 class="portfolio-section-title text-white margin-lg-0">Widget system</h3>
               <p class="portfolio-section-text text-grey-9c">
                 The dashboard widgets offer trainers an overview of all major components of their business needs.
                 It is an easy way to understand who are clients that need attention, scheduled activities,
                 clients' notifications, business growth.
               </p>
             </div>
-            <div class="col-12 col-lg-8 img-wrapper">
-              <img src="./../../assets/portfolios/trainerize/trz-dashboard.svg" alt="trainerize-dashborad">
+            <div class="col-12 col-lg-8 full-img-wrapper">
+              <img class="foo2" src="./../../assets/portfolios/trainerize/trz-dashboard.svg" alt="trainerize-dashborad">
             </div>
-            <div class="col-12">
-              <h3 class="portfolio-section-title text-white">Switching into</h3>
+          </div>
+          <div class="row flex-lg-row-reverse align-items-lg-center section-item-lg-margin-top">
+            <div class="col-12 col-lg-4 offset-lg-1">
+              <h3 class="portfolio-section-title text-white margin-lg-0">Switching into</h3>
               <p class="portfolio-section-text text-grey-9c">
                 Most of the functionalities of the web app are also available via mobile,
                 allowing trainers large flexibility when it comes to client management.
@@ -42,8 +46,8 @@
                 review stats, communicate and provide feedback to the clients, all on the go.
               </p>
             </div>
-            <div class="col-12 text-center img-wrapper">
-              <img src="./../../assets/portfolios/trainerize/trz-switching.png" width="579" height="530" alt="trainerize-switching">
+            <div class="col-12 col-lg-7 text-center img-wrapper">
+              <img src="./../../assets/portfolios/trainerize/trz-switching.png" class="margin-lg-0 foo3" alt="trainerize-switching">
             </div>
           </div>
         </div>
@@ -52,12 +56,98 @@
         <div class="container">
           <div class="row">
             <div class="col-12 text-center img-wrapper">
-              <img src="./../../assets/portfolios/trainerize/trz-laptop.svg" alt="trainerize-dashborad">
+              <img class="foo4" src="./../../assets/portfolios/trainerize/trz-laptop.svg" alt="trainerize-dashborad">
             </div>
             <div class="col-12 text-center img-wrapper">
               <img src="./../../assets/portfolios/trainerize/trz-visuals.svg" alt="trainerize-visuals">
             </div>
             <h2 class="col-12 portfolio-section-main-title text-center">Trainerize visuals</h2>
+          </div>
+          <div class="row align-items-lg-center">
+            <div class="col-12 col-lg-4">
+              <h3 class="portfolio-section-title margin-lg-0">Shapes & color</h3>
+              <p class="portfolio-section-text">
+                Clients' mobile app use is mostly intended while in the gym,
+                hence we developed colors and shapes system in which clients can easily,
+                at glance, understand their training plan structure, workout,
+                cardio or flexibility exercises, progress photos or measurement request etc.
+              </p>
+            </div>
+            <div class="col-12 col-lg-7 offset-lg-1 text-center">
+              <img class="foo5" src="./../../assets/portfolios/trainerize/trz-visuals-phone.svg" alt="trainerize visuals">
+            </div>
+            <div class="col-12 text-center">
+              <img src="./../../assets/portfolios/trainerize/trz-visuals-graphic.svg" alt="trainerize visuals">
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="trz-system-overview" class="trainerize-dark-bcg">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 top-wrapper text-center">
+              <h2 class="portfolio-section-main-title text-center">System overview</h2>
+              <img src="./../../assets/portfolios/trainerize/trz-system-overview-top.svg" alt="">
+            </div>
+          </div>
+          <div class="row flex-lg-row-reverse align-items-lg-center section-item-lg-margin-top">
+            <div class="col-12 col-lg-4">
+              <img src="./../../assets/portfolios/trainerize/trz-system-overview-graph.svg" class="d-none d-lg-block margin-bottom-40" alt="">
+              <h3 class="portfolio-section-title text-white margin-lg-0">Clients timeline</h3>
+              <p class="portfolio-section-text text-grey-9c">
+                These views serve to get understanding of the overall progress of the set training plan.
+                The timeline is also an overview of all activities, communication with the trainer and meal plans.
+              </p>
+            </div>
+            <div class="col-12 col-lg-8 text-center">
+              <img class="foo6" src="./../../assets/portfolios/trainerize/trz-system-overview-phones.svg" alt="">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-12 text-center">
+              <img class="foo7" src="./../../assets/portfolios/trainerize/trz-system-overview-human.svg" alt="">
+            </div>
+          </div>
+          <div class="row align-items-lg-center section-item-lg-margin-top">
+            <div class="col-12 col-lg-4">
+              <h3 class="portfolio-section-title text-white margin-lg-0">Progress feedback</h3>
+              <p class="portfolio-section-text text-grey-9c">
+                Besides the 3rd party integrations with Fitbit, Withings, MindBody and other products,
+                Trainierze allows clients to leave feedback for each of the exercises they tackled.
+                This simple yet powerful tool helps trainers understand the level of fitness and body
+                state of their clients hence assemble appropriate exercises.
+              </p>
+            </div>
+            <div class="col-12 col-lg-8 text-center">
+              <img class="foo8" src="./../../assets/portfolios/trainerize/trz-system-overview-screens.svg" alt="">
+            </div>
+          </div>
+          <div class="row flex-lg-row-reverse align-items-lg-center section-item-lg-margin-top">
+            <div class="col-12 col-lg-4 z-index-1">
+              <h3 class="portfolio-section-title text-white margin-lg-0">Tracking & workout assembily</h3>
+              <p class="portfolio-section-text text-grey-9c">
+                Trainerize comes with a library of hundreds of high-quality exercise videos,
+                enabling clients to easily understand and correctly perform given exercises.
+                Drag and drop system and ability to upload custom videos makes trainers job
+                easier and allows every gym and trainer to offer professional service.
+              </p>
+            </div>
+            <div class="col-12 col-lg-8 text-center full-img-wrapper">
+              <img class="foo9" src="./../../assets/portfolios/trainerize/trz-system-overview-laptop-mobile.svg" alt="">
+            </div>
+          </div>
+          <div class="row align-items-lg-center section-item-lg-margin-top">
+            <div class="col-12 col-lg-4">
+              <h3 class="portfolio-section-title text-white margin-lg-0">Complete communication system</h3>
+              <p class="portfolio-section-text text-grey-9c">
+                Trainerize features completed communication system available both on mobile and web app.
+                This tool allows trainers to assemble group and 1 on 1 communication channels with their clients,
+                allowing for ease and frequent communiction with the trainer during or off-training hours.
+              </p>
+            </div>
+            <div class="col-12 col-lg-7 offset-lg-1 full-img-wrapper text-center">
+              <img class="foo10" src="./../../assets/portfolios/trainerize/trz-system-overview-bottom.svg" alt="">
+            </div>
           </div>
         </div>
       </section>
@@ -86,17 +176,17 @@ export default {
   },
   methods: {
     attachScrollReveal () {
-      // window.sr = ScrollReveal({ reset: true })
-      // sr.reveal('.foo', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.2', reset: false}, 350)
-      // sr.reveal('.foo2', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.2', reset: false}, 350)
-      // sr.reveal('.foo3', { duration: 1200, scale: 1, distance:'70px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
-      // sr.reveal('.foo4', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
-      // sr.reveal('.foo6', { duration: 1200, scale: 1, distance:'70px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
-      // sr.reveal('.foo5', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
-      // sr.reveal('.foo7', { duration: 1200, scale: 1, distance:'70px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
-      // sr.reveal('.foo8', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
-      // sr.reveal('.foo9', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
-      // sr.reveal('.foo10', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
+      window.sr = ScrollReveal({ reset: true })
+      sr.reveal('.foo', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.2', reset: false}, 350)
+      sr.reveal('.foo2', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.2', reset: false}, 350)
+      sr.reveal('.foo3', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
+      sr.reveal('.foo4', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
+      sr.reveal('.foo6', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
+      sr.reveal('.foo5', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
+      sr.reveal('.foo7', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
+      sr.reveal('.foo8', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
+      sr.reveal('.foo9', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
+      sr.reveal('.foo10', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
       // sr.reveal('.foo11', { duration: 1200, scale: 1, distance:'70px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
       // sr.reveal('.foo12', { duration: 1200, scale: 1, distance:'70px', easing: 'ease', viewFactor:'0.3', reset: false}, 350)
       // sr.reveal('.foo13', { duration: 1200, scale: 1, distance:'30px', easing: 'ease', viewFactor:'0.7', reset: false}, 350)
